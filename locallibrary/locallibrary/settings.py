f8244712ab7a746e9505e19817864df8893d29d7
Lomkile Ntetha <lumkilentetha878@gmail.com>
2022-09-23 22:42:31 +0200

    update compose, settings, .env.dev

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = "locallibrary.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
